b4f64773e29e838590d16bdf5ccf446962bedeed
het4399 <het.sojitra4399@gmail.com>
2025-03-25 00:19:23 +0530

    Moved the user and blog into different folder

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,55 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_secret: string;
+  };
+}>();
+
+userRouter.post("/signup", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const response = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+  const payload = {
+    id: response.id,
+    role: "user",
+  };
+  // const jwtSecret = process.env.JWT_secret;
+  // if (!jwtSecret) {
+  //   throw new Error("JWT secret is not defined");
+  // }
+
+  const token: String = await sign(payload, c.env.JWT_secret);
+  return c.json({ token: token });
+});
+
+userRouter.post("/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({ where: { email: body.email, password: body.password } });
+  if (!user) {
+    c.status(401);
+    return c.json({ message: "Invalid credentials" });
+  }
+  const payload = {
+    id: user.id,
+    role: "user",
+  };
+  const token: string = await sign(payload, c.env.JWT_secret);
+
+  return c.json({ token: token });
+});
