e38a5d149c18055fae6d45d3cd9750557dc7c007
het4399 <het.sojitra4399@gmail.com>
2025-03-24 16:51:52 +0530

    Added prisma and initalise the structure of route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,34 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const prisma = new PrismaClient().$extends(withAccelerate())
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/user/signup", (c) => {
+  return c.text("Hello from Singup!");
+});
 
-export default app
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("Hello from Signin!");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("Hello from blog!");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hello from blog delete route!");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  const id=c.req.param('id');
+  console.log(id);
+  return c.text("Hello from blog id!");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("Hello from blog bulk route!");
+});
+
+export default app;
