8a81c3d5a2ea062c1ef7bb1dbaa81391ef107840
het4399 <het.sojitra4399@gmail.com>
2025-03-25 12:47:51 +0530

    Added common and added zod validation

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { sign } from "hono/jwt";
+import { SignUpInput, SignInInput } from "@het4399/common";
+
 export const userRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
@@ -11,6 +13,11 @@ export const userRouter = new Hono<{
 
 userRouter.post("/signup", async (c) => {
   const body = await c.req.json();
+  const { success, error } = SignUpInput.safeParse(body);
+  if (!success) {
+    c.status(400);
+    return c.json({ message: error.toString() });
+  }
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
@@ -40,6 +47,11 @@ userRouter.post("/signin", async (c) => {
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
+  const { success, error } = SignInInput.safeParse(body);
+  if (!success) {
+    c.status(400);
+    return c.json({ message: error.toString() });
+  }
   const user = await prisma.user.findUnique({ where: { email: body.email, password: body.password } });
   if (!user) {
     c.status(401);
