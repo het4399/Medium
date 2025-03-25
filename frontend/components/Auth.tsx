7614cf552e38026a8ca30afdee4a4d56a5b7a5c8
het4399 <het.sojitra4399@gmail.com>
2025-03-25 21:52:52 +0530

    Added auth ,quote ,sigup and sigin

--- frontend/components/Auth.tsx
+++ frontend/components/Auth.tsx
@@ -1,3 +1,73 @@
-export function Auth(){
-    return <>Hello</>;
+import { SignUpInput } from "@het4399/common";
+import { ChangeEventHandler, useState } from "react";
+import { Link } from "react-router-dom";
+
+export function Auth({ type }: { type: "SignUp" | "SignIn" }) {
+    const [postInputes, setPostInput] = useState<SignUpInput>({
+        email: "",
+        password: "",
+        name: "",
+    })
+    function sendRequest(req: Request, res: Response) {
+
+    }
+    return <>
+        <div className=' h-screen flex justify-center flex-col'>
+            <div className="flex justify-center">
+                <div>
+                    <div className="px-7">
+
+                        <div className="text-3xl font-extrabold">
+                            Create an account
+                        </div>
+                        <div className="text-slate-400 px-4 pt-1" >
+                            {type === "signin" ? "Don't have an account?" : "Already have an account?"}
+                            <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>
+                                {type === "signin" ? "Sign up" : "Sign in"}</Link>
+                        </div>
+                    </div>
+                    <div className="pt-1">
+
+                        {type === "signup" ? <LabelInput label={"Name"} placeholder={"Name"}
+                            onchange={(e) => {
+                                setPostInput({
+                                    ...postInputes,
+                                    name: e.target.value
+                                })
+                            }} /> : null}
+                        <LabelInput label={"Email"} placeholder={"Email"}
+                            onchange={(e) => {
+                                setPostInput({
+                                    ...postInputes,
+                                    email: e.target.value
+                                })
+                            }} />
+                        <LabelInput label={"Password"} placeholder={"Password"}
+                            onchange={(e) => {
+                                setPostInput({
+                                    ...postInputes,
+                                    password: e.target.value
+                                })
+                            }} />
+                        <button  type="button" className="mt-5 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>;
 };
+interface LabelInputType {
+    label: string;
+    onchange: (e: ChangeEventHandler<HTMLInputElement>) => void;
+    placeholder: string;
+    type?: string;
+}
+function LabelInput({ label, placeholder, onchange, type }: LabelInputType) {
+    return <> <div className="mb-1">
+        <label className="block mb-2 text-sm text-black font-semibold pt-3">{label}</label>
+        <input onChange={onchange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+    </div>
+    </>
+}
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,9 +3,11 @@ import { Auth } from '../../components/Auth';
 
 export const Signup = () => {
     return <>
-        <div className='grid grid-cols-2'>
-            <Auth />
-            <div className='invisible lg:visible'>
+        <div className='grid grid-cols-1 lg:grid-cols-2'>
+            <Auth type='signup' />
+            <div className='hidden lg:block'>
                 <Quote />
-            </div>        </div></>
+            </div>
+        </div>
+    </>
 }
