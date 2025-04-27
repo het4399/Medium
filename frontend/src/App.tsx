f29180307a2f30fce30b8103edfe4f04a9ee2f30
het4399 <het.sojitra4399@gmail.com>
2025-04-27 15:06:38 +0530

    Added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,12 @@ import { Signup } from './pages/Signup'
 import { Blog } from './pages/Blog'
 import { Signin } from './pages/Signin'
 import { Blogs } from './pages/Blogs'
-import { Publish } from './components/Publish'
+import { Publish } from './pages/Publish'
+import { Home } from './pages/Home'
+import { AboutUs } from './pages/About'
+import { Settings } from './pages/Settings'
+import { Profile } from './pages/Profile'
+import { Update } from './pages/Update'
 
 
 function App() {
@@ -12,12 +17,16 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/publish" element={<Publish />} />
-          
+          <Route path='/edit/:id' element={<Update />} />
+          <Route path='/about' element={<AboutUs />} />
+          <Route path='/settings' element={<Settings />} />
+          <Route path='/profile' element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </>
